ac03280ffb31c5d903292513c22eabd490e2499d
Francisco <j.f.maurino@gmail.com>
2023-12-24 20:30:26 +0000

    Agrego docker y compose files.

--- frontend/intuitivoaichallenge/next.config.js
+++ frontend/intuitivoaichallenge/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8001/api' // development api
-        : 'http://localhost:8001/api' // production api
+        ? process.env.BACK_URL || 'http://localhost:8001/api' // development api
+        : process.env.BACK_URL || 'http://localhost:8001/api' // production api
 }
 }
